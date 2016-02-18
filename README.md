ccdd2146e3d8cbd8e956d16b822992bbb59b1d8c
biezhi <biezhi.me@gmail.com>
2016-02-18 15:30:26 +0800

    -

---
+++ README.md
@@ -0,0 +1,13 @@
+# blog
+
+## Run
+
+create db `blog`, import [blog.sql](blog.sql)
+
+```sh
+git clone https://github.com/blade-samples/blog.git
+mvn jetty:run
+```
+
+Access URL http://127.0.0.1:9000
+
---
+++ blog.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.40 : Database - blog
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `t_post` */
+
+DROP TABLE IF EXISTS `t_post`;
+
+CREATE TABLE `t_post` (
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `content` text,
+  `view_count` int(10) DEFAULT '0',
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_post` */
+
+insert  into `t_post`(`id`,`title`,`content`,`view_count`,`create_time`) values (1,'Hello Blade','bbbbbbbbbbbbbbbbbbb',0,'2016-02-18 14:15:59');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
